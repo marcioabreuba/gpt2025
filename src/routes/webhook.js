0cd09ce3e0c846a0eb565ef38961dbc309b98c01
Marcio Abreu <marcioabreu.ba@gmail.com>
2025-02-25 08:39:02 -0300

    audio

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import { getChat } from '../services/conversationService.js';
 import { processAudioMessage } from '../services/audioService.js';
-import { sendAudioReceiptConfirmation } from './zapiService.js';
+import { sendAudioReceiptConfirmation } from '../services/zapiService.js';
 import logger from '../utils/logger.js';
 
 const router = express.Router();
